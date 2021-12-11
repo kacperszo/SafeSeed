057958484d26b3ec3940e19e515e8a13cf3dc52f
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-12-11 09:07:55 +0100

    Add theming config

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 className="text-2xl text-blue-200">Hello</h1>
+      <h1 className="text-2xl text-katowice">Hello</h1>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "beige": "#F5F2EC",
+        "orange": "#F76E64",
+        "deep-purple": "#3E1741",
+        "katowice": "#796C7A",
+        "relish": "#0C442D"
+      }
+    },
   },
   plugins: [],
 }
