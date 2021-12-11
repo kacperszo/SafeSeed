03a0e9b5dcd66694ac51b9b2665777e756ae5c18
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-12-11 14:54:54 +0100

    Add basic chat screen

---
+++ src/features/Dashboard/components/ChatMessage.jsx
@@ -0,0 +1,14 @@
+import clsx from "clsx"
+
+const ChatMessage = ({incoming, message}) => {
+    return (
+        <div className={clsx("bg-white text-black p-4 w-56 rounded-xl mb-1 mt-1",{
+            "rounded-bl-none self-start mr-auto":incoming,
+            "ml-auto rounded-br-none": !incoming            
+        })} >
+            {message}
+        </div>
+    )
+}
+
+export default ChatMessage
