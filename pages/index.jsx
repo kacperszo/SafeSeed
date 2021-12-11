2d341dcddcafc7ec1f29908f0e9d233b402ad4ba
Maciej Wiatr <maciekwiatr17@gmail.com>
2021-12-11 09:26:21 +0100

    Change filetypes from js to jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,10 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <div>
+
       <h1 className="text-2xl text-deep-purple font-quirky font-black">SafeSeed</h1>
+      </div>
     </div>
   )
 }
