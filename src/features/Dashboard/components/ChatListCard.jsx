1199b13ca1e47fcc96723d6d7f3ea2d3c5ff216f
Jakub WIlk <wilkjakub64@gmail.com>
2021-12-12 04:50:44 +0100

    Add cursor: pointer to ChatListCard

--- src/features/Dashboard/components/ChatListCard.jsx
+++ src/features/Dashboard/components/ChatListCard.jsx
@@ -2,12 +2,14 @@ import Card from './Card'
 
 const ChatListCard = ({ username, tagline, onClick }) => {
  return (
-  <Card>
-   <div onClick={onClick} className="p-4">
-    <h2 className="font-quirky font-extrabold">{username}</h2>
-    <p className="text-katowice">{tagline}</p>
-   </div>
-  </Card>
+     <div className="hover:cursor-pointer">
+        <Card>
+        <div onClick={onClick} className="p-4">
+        <h2 className="font-quirky font-extrabold">{username}</h2>
+        <p className="text-katowice">{tagline}</p>
+        </div>
+        </Card>
+     </div>
  )
 }
 
